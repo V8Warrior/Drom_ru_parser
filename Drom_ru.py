25c0a678540caecc817a7aca0b8414ab2f8b3586
Niki <v8warrior@yandex.ru>
2021-06-25 01:41:06 +0300

    Working version. Collects data cash. Getting data separated on 2 functions

--- Drom_ru.py
+++ Drom_ru.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests as req
 import numpy as np
 import pandas as pd
+import os.path as osp
 import re
 import json
 
@@ -243,10 +244,10 @@ class drom:
                 dat['year'] = year
                 dat['price'] = price
                 dat['link'] = link
-        return dat
+                return dat
 
-    def errlog(self, file):
-        with open('err_drom.txt', 'w') as f:
+    def errlog(self, file, city):
+        with open(f'err_drom_{city}.txt', 'w') as f:
             f.write(str(file))
 
     def get_data(self, brand='all', model='all', city=77):
@@ -288,19 +289,26 @@ class drom:
             except UnicodeDecodeError:
                 errlist.append([row, 'Unicode_error'])
                 print(f'Проблема в {row}')
-                self.errlog(errlist)
+                self.errlog(errlist, city)
             except AttributeError:
                 errlist.append([row, 'NoneType'])
                 print(f'Проблема в {row}')
-                self.errlog(errlist)
-
+                self.errlog(errlist, city)
         print('Удаление строк без ссылок...')
         cardat = cardat.dropna(subset=['link'])
         print('Распределение по строкам...')
         cardat = cardat.explode(column='link')
 
+        # сохранение кэша
+        print(f'Кэширование региона с кодом {city}')
+        cardat.to_csv(f'parsers\drom_ru_region_{city}_cash.csv')
+        return cardat
+
+    def unpack_links(self, cardat):
+        global errlist
+        city = cardat.reg_id.values[0]
         linkdata = pd.DataFrame([])
-        links = cardat.link.dropna().values
+        links = cardat.link.values
         if links.size !=0:
             print('Процесс сборки данных с объявлений...')
             for i, link in enumerate(links):
@@ -310,15 +318,15 @@ class drom:
                 except UnicodeDecodeError:
                     errlist.append([link, 'Unicode_error'])
                     print(f'Проблема в {link}')
-                    self.errlog(errlist)
+                    self.errlog(errlist, city)
                 except AttributeError:
                     errlist.append([link, 'NoneType'])
                     print(f'Проблема в {link}')
-                    self.errlog(errlist)
+                    self.errlog(errlist, city)
                 cardat = cardat.merge(linkdata, on='link')
         else:
             errlist.append([f'No links in region: {city}'])
-            self.errlog(errlist)
+            self.errlog(errlist, city)
 
         filename = f'parsers\cars_drom_region_{city}.csv'
         cardat.to_csv(filename)
@@ -329,225 +337,9 @@ regions = drom.regions.index.values
 errlist = []
 for i, code in enumerate(regions):
     print(code)
-    print(f'регион {i+1} / {regions.shape[0]}')
-    drom.get_data(brand='all', city=code)
-# with open('err_drom.txt', 'r') as f:
-#     f.read()
-# l = [np.array(['1', '2']), ['3'], ['4']]
-# print(str(l))
-# with open('test.txt', 'w') as f:
-#     f.write(str(l))
-# with open('errlist.txt', 'r') as f:
-#     print(f.read())
-# print(np.fromfile('errlist.npy'))
-# for
-
-# print(drom.get_car_data(link))
-# print(drom.get_full_link(string=['Audi', 'A5', 'Москва']))
-# Распределение данных по ссылкам
-# dat = pd.DataFrame([], columns=['link'])
-# dat.link = drom.get_data_links(brand='Audi', model='A5', city='Москва')
-# dat2 = pd.DataFrame([])
-# for link in dat.link.values:
-#     dat2 = dat2.append(drom.get_car_data(link))
-# print(dat.merge(dat2, on='link'))
-
-# print(drom.get_full_link(brand='Land Rover', model='Range Rover', city='Санкт-Петербург'))
-
-# # print(drom.get_all_cars())
-# print(drom.get_sale_links('ГАЗ', '3110 Волга', "Москва"))
-
-
-
-# dat = pd.DataFrame([['ГАЗ', '3110 Волга', "Москва"]], columns=['brand', 'model', 'city'])
-# dat['link'] = [drom.get_sale_links('ГАЗ', '3110 Волга', "Москва")]
-# dat = dat.explode('link')
-# dat2 = pd.DataFrame([])
-# for link in dat.link:
-#     dat2 = dat2.append(drom.get_car_data(link))
-# dat = dat.merge(dat2, on='link')
-# print(dat)
-
-# print(drom.all_cars)
-
-# cars = drom.get_all_cars().head(3)
-# cars.city = 'Москва'
-# cars.to_csv('cars.csv')
-
-# cars = pd.read_csv('cars.csv', index_col=0)
-# dat = pd.DataFrame([])
-# cars.link = cars.link.astype('object')
-# for i in cars.index:
-#     car = cars.iloc[i]
-#     linklist = drom.get_sale_links(car.brand, car.model, car.city)
-#     if linklist.size != 0:
-#         cars.at[i, 'link'] = linklist
-# cars = cars.explode('link')
-# print(cars)
-# Загатовка
-# drom.get_all_cars().to_csv('all_cars_models.csv')
-# print('Файл есть')
-
-
-# cars.link = [cars[['brand', 'model', 'city']].apply(drom.get_sale_links)]
-# print(cars)
-# link = 'https://moscow.drom.ru/uaz/patriot/43021634.html'
-# print(drom.get_car_data(link))
-# dat = drom.get_all_cars()
-# links = drom.get_sale_links()
-# dat.loc['Audi'].query('model == "A4"')['link'].fillna([['l', 'b']])
-# dat = dat.explode(column='link')
-# print(dat.loc['Audi'].query('model == "A4"')['link'].values)
-
-
-
-# dat = drom.get_all_cars()
-# arr = drom.get_sale_links()
-# filter = ((dat.model == 'A4') & (dat.brand == 'Audi'))
-# idx = dat[filter].index
-# dat.loc[idx, 'link'] = [arr]
-# print(dat.query('brand == "Audi"'))
-
-
-
-# def get_html(url):
-#     with req.get(url, headers=HEADERS) as r:
-#         # r.content.decode('utf-8-sig')
-#         r.encoding
-#     return r
-#
-# def get_content_stage_1(html):
-#     soup = BeautifulSoup(html.text, 'html.parser')
-#     # items = soup.find_all('div', class_='css-u4n5gw ebqjjri4')
-#     items = soup.find('div', class_='css-2u02es ete74kl0').find_all('noscript' and 'a')
-#     # items = find.find_all('a')
-#     links = []
-#     for item in items:
-#         find = item.get('href')
-#         if find != []:
-#             links.append(find)
-#     #     links.append(item.find('a',  class_='css-171rdfx').get('href'))
-#     # #     # brands.append(item.find('div', class_='css-1p05nxt').get_text())
-#     return links #brands
-#
-# def get_content_stage_2(html):
-#     soup = BeautifulSoup(html.text, 'html.parser')
-#     items = soup.find_all('a', class_='e64vuai1')
-#     # links = []
-#     ends = []
-#     # model_names = []
-#     for item in items:
-#         # links.append(item.get('href'))
-#         # model_names.append(item.get_text())
-#         ends.append(item.get('href')[28:])
-#     return ends #, model_names
-#
-# def get_content_stage_3(html):
-#     soup = BeautifulSoup(html.text, 'html.parser')
-#     items = soup.find_all('a', class_='css-1hgk7d1')
-#     strings = len(soup.find_all('div', class_='css-12xjs1i e15hqrm30'))
-#     cars = []
-#     for item in items:
-#         cars.append(item.get('href'))
-#     return cars
-#
-# def get_content_stage_4(link):
-#     global errlist
-#     html = get_html(link)
-#     if html.status_code == 200:
-#         soup = BeautifulSoup(html.text, 'html.parser')
-#         car_data = {}
-#         # errlist = []
-#         try:
-#             fullname = re.findall(r'[/](\w+)',link)
-#             # fullname = re.findall(r'\w+', soup.find('h1', class_='css-cgwg2n').get_text()) #css-cgwg2n
-#             char_items = soup.find_all('tr', class_='css-10191hq')
-#             try:
-#                 car_data['name'] = fullname[1]
-#                 car_data['model'] = fullname[2]
-#             except IndexError:
-#                 print(f'Что-то не так с названием, не распределяет по индексам')
-#                 errlist.append({'index_err': link})
-#
-#             for item in char_items:
-#                 key = item.find('th', class_='css-k5ermf')
-#                 value = item.find('td',class_='css-1uz0iw8 ezjvm5n1')
-#                 if key != None and value != None:
-#                     car_data[key.get_text()] = value.get_text()
-#             try:
-#                 car_data['price'] = int(''.join(re.findall(r'\d+', soup.find('div', class_='css-1hu13v1').get_text())))
-#                 car_data['link'] = link
-#             except AttributeError:
-#                 print(f'\nfullname - None (Данные цены не прочитались) in {link}\n')
-#                 errlist.append({'price_err': link})
-#             return car_data
-#         except AttributeError:
-#             print(f'\nfullname - None (Данные имени не прочитались) in {link}\n')
-#             errlist.append({'nonename' : link})
-#             return None
-#     else:
-#         print(f'Проблема с ссылккой {link}')
-#         errlist.append({'connection_problem' : link})
-#
-#
-# def saver(doc, name= ""):
-#     with open(f'parsing_data\{name}_drom_ru.json', 'w') as file:
-#         json.dump(doc, file)
-#
-# def error_saver(errlist):
-#     with open('parsing_data\err_drom_ru.json', 'w') as file:
-#         json.dump(errlist, file)
-#
-# def parsing_cars(url1, page1=0, page2=1, brandstart=0):
-#     global errlist
-#     # connect
-#     html = get_html(url1)
-#     if html.status_code == 200:
-#         brandlist = get_content_stage_1(html)
-#         # cars = []
-#         for brand in brandlist[brandstart:]:
-#             html_brand = get_html(brand)
-#             if html_brand.status_code == 200:
-#                 modellist = get_content_stage_2(html_brand)
-#                 brandname = re.findall(r'\w+', brand[28:])[0]
-#                 print(brandname)
-#                 cars_brand = []
-#                 for ends in modellist:
-#                     name_model = re.findall(r'\w+', ends)
-#                     carurl = 'https://auto.drom.ru/' + ends + 'used/'
-#                     print(' '.join(name_model))
-#                     for page in range(page1, page2 +1):
-#                         url3 = carurl + f'page{page}/'
-#                         html_car = get_html(url3)
-#                         if html_car.status_code == 200:
-#                             print(f'Парсинг страницы: {page}')
-#                             carlist = get_content_stage_3(html_car)
-#                             if carlist == []:
-#                                 print('Данные по модели есть')
-#                                 break
-#                             for link in carlist:
-#                                 print(link)
-#                                 car = get_content_stage_4(link)
-#                                 if car != None:
-#                                     cars_brand.append(car)
-#                                     error_saver(errlist)
-#                                     # cars.append(car)
-#                                 saver(cars_brand, name=brandname)
-#                                 # saver(cars, name='all')
-#                         else:
-#                             print(f'Проблема с ссылккой {url3}')
-#                             errlist.append({'connection_problem': url3})
-#                             error_saver(errlist)
-#             else:
-#                 print(f'Проблема с ссылккой {brand}')
-#                 errlist.append({'connection_problem': brand})
-#                 error_saver(errlist)
-
-# # ТЕСТ
-# errlist = []
-# par = parsing_cars(URL, 1, 101, -7)
-
-# url1 = 'https://ust-tarka.drom.ru/lada/2121_4x4_niva/41666637.html'
-# url2 = 'https://spb.drom.ru/land_rover/range_rover/41748748.html'
-# find = get_content_stage_4(url2)
-# print(find)
+    print(f'регион {i + 1} / {regions.shape[0]}')
+    if osp.exists(f'parsers/drom_ru_region_{code}_cash.csv') == False:
+        drom.get_data(brand='all', city=code)
+    else:
+        dat = pd.read_csv(f'parsers/drom_ru_region_{code}_cash.csv')
+        drom.unpack_links(dat)
